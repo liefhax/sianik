39dc8cba8c1bb0616df56c48c4e29f74405bb96d
alief <122020553+liefhax@users.noreply.github.com>
2025-01-19 22:30:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ANTRIAN</title>
+    <link rel="stylesheet" href="vendor/css/index.css">
+</head>
+<div class="reception-area">
+        <div class="reception-overlay"></div>
+    </div>
+    <div class="login-container">
+        <div class="login-box">
+            <h1>Selamat Datang di SIANIK 😊</h1>
+            <div class="button-group">
+                <button onclick="window.location.href='user/login.php'">Login</button>
+                <button onclick="window.location.href='user/register.php'">Register</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
